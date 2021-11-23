652b539a7e05a83aafe5b3f773b8b545a08dee92
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-11-22 22:45:00 -0300

    m changes

--- covid-app.py
+++ covid-app.py
@@ -22,7 +22,7 @@ app_cov_frame= Frame(window, width=840, height=50, background=c1, relief="flat")
 app_cov_frame.grid(row=0, column=0, columnspan=3, sticky=NSEW)
 
 #covid infected frame
-send_frame_infected= Frame(window, width=220, height=100, background=c3, relief="flat")
+send_frame_infected= Frame(window, width=220, height=100, background=c2, relief="flat")
 send_frame_infected.grid(row=1, column=0, sticky=NW, pady=5, padx=5)
 
 #covid retrieved frame
@@ -52,13 +52,17 @@ label_infected = Label(send_frame_infected, text="5.126", width=12, height=1, pa
                        relief="flat", anchor=NW, font=("Courier 25 bold"), background=c2, fg=c0)
 label_infected.grid(row=1, column=0, pady=1)
 
-label_infected = Label(send_frame_infected, text="Segunda-Feira 22/11/2021", width=33, height=1, pady=7, padx=0, 
+send_forms = Label(send_frame_infected, text="Segunda-Feira 22/11/2021", width=25, height=1, pady=7, padx=0, 
                        relief="flat", anchor=NW, font=("Courier 11 bold"), background=c2, fg=c0)
-label_infected.grid(row=2, column=0, pady=1)
+send_forms.grid(row=2, column=0, pady=1)
 
-label_infected = Label(send_frame_infected, text="Total de casos ativos de covid-19", width=33, height=1, pady=7, padx=0, 
+send_forms = Label(send_frame_infected, text="Total de casos ativos de covid-19", width=33, height=1, pady=7, padx=0, 
                        relief="flat", anchor=NW, font=("Courier 8 bold"), background=c2, fg=c0)
-label_infected.grid(row=3, column=0, pady=1)
+send_forms.grid(row=3, column=0, pady=1, padx=13)
+
+lin_blue = Label(send_frame_infected, text="", width=19, height=1, pady=1, padx=0, 
+                       relief="flat", anchor=NW, font=("Courier 1 bold"), background=c3, fg=c0)
+lin_blue.grid(row=4, column=0, pady=0, padx=0, sticky=NSEW)
 
 
 window.mainloop()
