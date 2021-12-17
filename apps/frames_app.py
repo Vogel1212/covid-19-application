0d58d8275edbcc1f569ce42b8e1ce2c3f91a2e54
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-12-17 01:25:45 -0300

    m changes

--- apps/frames_app.py
+++ apps/frames_app.py
@@ -31,28 +31,29 @@ window.resizable(width=FALSE, height=FALSE)
 window.geometry('835x360')
 window.configure(background=c6)
 
-class Frames:
-        def __init__(self) -> None:
-                label_infected = Label(send_frame_infected, text="Infected", width=20, height=1, pady=7, padx=0, 
-                                relief="flat", anchor=NW, font=("Courier 15 bold"), background=c2, fg=c0)
-                label_infected.grid(row=0, column=0, pady=1, padx=13)
+class Covid_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(835, 360)
+        label_infected = Label(send_frame_infected, text="Infected", width=20, height=1, pady=7, padx=0, 
+                        relief="flat", anchor=NW, font=("Courier 15 bold"), background=c2, fg=c0)
+        label_infected.grid(row=0, column=0, pady=1, padx=13)
 
-                label_infected = Label(send_frame_infected, text=infected, width=12, height=1, pady=7, padx=0, 
-                                relief="flat", anchor=NW, font=("Courier 25 bold"), background=c2, fg=c0)
-                label_infected.grid(row=1, column=0, pady=1)
+        label_infected = Label(send_frame_infected, text=infected, width=12, height=1, pady=7, padx=0, 
+                        relief="flat", anchor=NW, font=("Courier 25 bold"), background=c2, fg=c0)
+        label_infected.grid(row=1, column=0, pady=1)
 
-                send_forms = Label(send_frame_infected, text=str(day), width=25, height=1, pady=7, padx=0, 
-                                relief="flat", anchor=NW, font=("Courier 11 bold"), background=c2, fg=c0)
-                send_forms.grid(row=2, column=0, pady=1)
+        send_forms = Label(send_frame_infected, text=str(day), width=25, height=1, pady=7, padx=0, 
+                        relief="flat", anchor=NW, font=("Courier 11 bold"), background=c2, fg=c0)
+        send_forms.grid(row=2, column=0, pady=1)
 
-                send_forms = Label(send_frame_infected, text="Total active covid-19 cases", width=33, height=1, pady=7, padx=0, 
-                                relief="flat", anchor=NW, font=("Courier 8 bold"), background=c2, fg=c0)
-                send_forms.grid(row=3, column=0, pady=1, padx=13)
+        send_forms = Label(send_frame_infected, text="Total active covid-19 cases", width=33, height=1, pady=7, padx=0, 
+                        relief="flat", anchor=NW, font=("Courier 8 bold"), background=c2, fg=c0)
+        send_forms.grid(row=3, column=0, pady=1, padx=13)
 
-                lin_blue = Label(send_frame_infected, text="", width=19, height=1, pady=1, padx=0, 
-                                relief="flat", anchor=NW, font=("Courier 1 bold"), background=c3, fg=c0)
-                lin_blue.grid(row=4, column=0, pady=0, padx=0, sticky=NSEW)
-        
+        lin_blue = Label(send_frame_infected, text="", width=19, height=1, pady=1, padx=0, 
+                        relief="flat", anchor=NW, font=("Courier 1 bold"), background=c3, fg=c0)
+        lin_blue.grid(row=4, column=0, pady=0, padx=0, sticky=NSEW)
         
 def send_frame_recovered(recovereds):
         label_recovered = Label(send_frame_recovered, text="Recovered", width=20, height=1, pady=7, padx=0, 
